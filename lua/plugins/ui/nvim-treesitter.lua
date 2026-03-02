676909de786721c33d7cdfb3a3137959a852e2d1
= <exxlf@proton.me>
2026-03-02 12:16:50 +0100

    Fix nvim-treesitter config

--- lua/plugins/ui/nvim-treesitter.lua
+++ lua/plugins/ui/nvim-treesitter.lua
@@ -1,15 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-      sync_install = false,
-      auto_install = false,
-
-      highlight = {
-        enable = true,
-      },
-    })
-  end,
+  opts = {
+    ensure_installed = { "lua", "vim", "vimdoc", "query" },
+    highlight = { enable = true },
+    indent = { enable = true },
+    auto_install = true,
+  },
 }
